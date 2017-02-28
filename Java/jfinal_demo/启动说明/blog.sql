d4255a31d5e944c584801ff72eb7ce29d0efc6f1
dudustin <452728860@qq.com>
2017-03-01 01:09:31 +0800

    hello (#2)
    * Update blog.sql
    
    * 添加了git忽略文件 (#1)
    
    * update the mysql

--- Java/jfinal_demo/启动说明/blog.sql
+++ Java/jfinal_demo/启动说明/blog.sql
@@ -4,13 +4,13 @@ USE jfinal_demo;
 
 CREATE TABLE `blog` (
   `id` int(11) NOT NULL auto_increment,
-  `title` varchar(200) NOT NULL,
-  `content` mediumtext NOT NULL,
+  `head` varchar(200) NOT NULL,
+  `paper` mediumtext NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `blog` VALUES ('1', 'JFinal Demo Title here', 'JFinal Demo Content here');
+INSERT INTO `blog` VALUES ('1', 'JFinal Demo Headline here', 'JFinal Demo Paper here');
 INSERT INTO `blog` VALUES ('2', 'test 1', 'test 1');
 INSERT INTO `blog` VALUES ('3', 'test 2', 'test 2');
 INSERT INTO `blog` VALUES ('4', 'test 3', 'test 3');
